7043d823b4cda28e7bb85a6a0baadea3a1e7b799
shahi <shahi@10.0.0.31>
2022-07-02 23:12:36 -0500

    Imran

---
+++ True_Batch27/src/test/java/org/tn/java/project/CheckingAccount.java
@@ -0,0 +1,26 @@
+package org.tn.java.project;
+
+public class CheckingAccount {
+
+	static int balance=1000;
+	static int withdraw=500;
+	
+			
+	//*public void account() {
+				
+				
+	//			balance=1000;
+	//			withdraw=500;
+	//		}
+			
+	public static void main(String[] args) {
+
+		
+		int total=balance-withdraw;
+		System.out.println("this is my balance $" +total);	
+		
+	}		
+			
+			
+			
+} 
---
+++ True_Batch27/src/test/java/org/tn/java/project/SavingAccount.java
@@ -0,0 +1,5 @@
+package org.tn.java.project;
+
+public class SavingAccount {
+
+}
